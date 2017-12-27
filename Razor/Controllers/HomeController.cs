334e29a5de8f3c23bb1a9237546d6d6923f94b13
Saeed Shahani <dariusstyxedge@gmail.com>
2017-12-27 18:10:41 +0000

    Add files via upload

---
+++ Razor/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Razor.Models;
+
+namespace Razor.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            Product[] array =
+            {
+                new Product {Name = "Kayak", Price = 275M, ProductID = 1 },
+                new Product {Name = "Lifejacket", Price = 48.95M, ProductID = 2 },
+                new Product {Name = "Football", Price = 19.50M, ProductID = 3 },
+                new Product {Name = "Corner flag", Price = 34.95M, ProductID = 4 }
+            };
+
+            ViewBag.StockLevel = 2;
+
+            return View(array);
+        }
+    }
+}
